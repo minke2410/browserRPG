dd81b029a4c8576c2e05c14fe7420477073cdf39
minke <massa71524@gmail.com>
2025-01-13 21:10:35 +0900

    db.phpの編集

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 class Database {
     private $host = 'localhost';
-    private $dbname = 'rpg';
-    private $username = 'minke';
-    private $password = 'Usami71524!';
+    private $dbname = 'ブラウザRPG';
+    private $username = 'root';
+    private $password = 'Usami71524';
     private $pdo;
 
     public function __construct() {
